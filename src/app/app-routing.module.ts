a27600478a98e6c864aa695d033eb5f41146b5c9
Maxim Ilin <mqxim@icloud.com>
2021-08-17 00:37:49 +0300

    Updated routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,18 +1,36 @@
-import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import {router as signInRouter} from './components/sign-in/sign-in-routing.module';
-import {router as signUpRouter} from './components/sign-up/sign-up-routing.module';
-import {router as inAppRouter} from './components/in-app/in-app/in-app-routing.module';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { router as signInRouter } from './components/sign-in/sign-in-routing.module';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { InAppComponent } from './components/in-app/in-app/in-app.component';
+import { UserAccountComponent } from './components/in-app/user-account/user-account.component';
+import { AccountReportComponent } from './components/in-app/account-report/account-report.component';
 
 const routes: Routes = [
-  ...inAppRouter,
+  {
+    path: '',
+    component: InAppComponent,
+    children: [
+      {
+        path: '',
+        component: UserAccountComponent,
+      },
+      {
+        path: 'account/:id',
+        component: AccountReportComponent
+      }
+    ]
+  },
   ...signInRouter,
-  ...signUpRouter
+  {
+    path: 'sign-up',
+    component: SignUpComponent
+  }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
 export class AppRoutingModule {
 }
